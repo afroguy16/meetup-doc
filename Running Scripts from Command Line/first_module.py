1afcd448a73c4b391ddc8d9e6341f4704afb6685
Tijesunimi Peters <tijesunimi48@gmail.com>
2016-02-07 19:24:25 +0100

    Peters Commit

---
+++ Running Scripts from Command Line/first_module.py
@@ -0,0 +1,17 @@
+def first_module():
+    print 'This is my First Module'
+    
+    
+def magic():
+    print 'There is magic in falling in love'
+    
+
+def friends():
+    print 'if you have no friends, then you are a boring person'
+    
+    
+def guru():
+    print 'i am going to be a guru very soon'
+    
+def guguru():
+    print 'This is the local Popcorn and it can either be sweet or bland'
